8fc2cead9b02311788bb5e67cfc8e41ef5d2bfba
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-06-14 21:10:33 +0200

    Further integrate the home page

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,9 +1,17 @@
+<?php use_stylesheet('home') ?>
+
 <?php echo image_tag('slideshow_placeholder.jpg') ?>
 
 <h2>news</h2>
-<hr />
-<span class="date">03.09.2009</span>
-<p class="article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed sem nunc. Mauris sem tellus, faucibus eu accumsan sed, mattis sit amet nunc. Nunc sed est eget enim auctor viverra. Integer quis sem tempus est ornare porttitor. Suspendisse potenti. In a mauris orci, vel auctor augue. In sit amet dui ac dui gravida posuere. Morbi eu ante enim, eu convallis metus. Fusce et leo nunc, nec feugiat tortor. Integer nulla risus, faucibus sed mollis eget, placerat vitae purus. Etiam dictum, ante a suscipit fermentum, dolor diam ultrices libero, vel posuere ligula nibh in ipsum. Nam ac justo nec est mollis imperdiet ut id nunc. Mauris et elit nisl, quis feugiat justo. Quisque massa magna, pulvinar non sodales ac, tincidunt nec dui. Aliquam dignissim tellus in neque sodales id fringilla dolor faucibus. Donec ultrices posuere sem, at tristique dolor eleifend vitae. Curabitur vitae mauris vitae orci placerat suscipit eu hendrerit felis. Suspendisse vestibulum lacinia lectus, eget adipiscing dui lobortis eu.</p>
-<hr />
-<span class="date">03.09.2009</span>
-<p class="article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed sem nunc. Mauris sem tellus, faucibus eu accumsan sed, mattis sit amet nunc. Nunc sed est eget enim auctor viverra. Integer quis sem tempus est ornare porttitor. Suspendisse potenti. In a mauris orci, vel auctor augue. In sit amet dui ac dui gravida posuere. Morbi eu ante enim, eu convallis metus. Fusce et leo nunc, nec feugiat tortor. Integer nulla risus, faucibus sed mollis eget, placerat vitae purus. Etiam dictum, ante a suscipit fermentum, dolor diam ultrices libero, vel posuere ligula nibh in ipsum. Nam ac justo nec est mollis imperdiet ut id nunc. Mauris et elit nisl, quis feugiat justo. Quisque massa magna, pulvinar non sodales ac, tincidunt nec dui. Aliquam dignissim tellus in neque sodales id fringilla dolor faucibus. Donec ultrices posuere sem, at tristique dolor eleifend vitae. Curabitur vitae mauris vitae orci placerat suscipit eu hendrerit felis. Suspendisse vestibulum lacinia lectus, eget adipiscing dui lobortis eu.</p>
+<article class="news">
+    <span class="date">03.09.2009</span>
+    <div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed sem nunc. Mauris sem tellus, faucibus eu accumsan sed, mattis sit amet nunc. Nunc sed est eget enim auctor viverra. Integer quis sem tempus est ornare porttitor. Suspendisse potenti. In a mauris orci, vel auctor augue. In sit amet dui ac dui gravida posuere. Morbi eu ante enim, eu convallis metus. Fusce et leo nunc, nec feugiat tortor. Integer nulla risus, faucibus sed mollis eget, placerat vitae purus. Etiam dictum, ante a suscipit fermentum, dolor diam ultrices libero, vel posuere ligula nibh in ipsum. Nam ac justo nec est mollis imperdiet ut id nunc. Mauris et elit nisl, quis feugiat justo. Quisque massa magna, pulvinar non sodales ac, tincidunt nec dui. Aliquam dignissim tellus in neque sodales id fringilla dolor faucibus. Donec ultrices posuere sem, at tristique dolor eleifend vitae. Curabitur vitae mauris vitae orci placerat suscipit eu hendrerit felis. Suspendisse vestibulum lacinia lectus, eget adipiscing dui lobortis eu.</div>
+</article>
+<article class="news">
+    <span class="date">03.09.2009</span>
+    <div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+</article>
+<article class="news">
+    <span class="date">03.09.2009</span>
+    <div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed sem nunc. Mauris sem tellus, faucibus eu accumsan sed, mattis sit amet nunc. Nunc sed est eget enim auctor viverra. Integer quis sem tempus est ornare porttitor. Suspendisse potenti. In a mauris orci, vel auctor augue. In sit amet dui ac dui gravida posuere. Morbi eu ante enim, eu convallis metus. Fusce et leo nunc, nec feugiat tortor. Integer nulla risus, faucibus sed mollis eget, placerat vitae purus. Etiam dictum, ante a suscipit fermentum, dolor diam ultrices libero, vel posuere ligula nibh in ipsum. Nam ac justo nec est mollis imperdiet ut id nunc. Mauris et elit nisl, quis feugiat justo. Quisque massa magna, pulvinar non sodales ac, tincidunt nec dui. Aliquam dignissim tellus in neque sodales id fringilla dolor faucibus. Donec ultrices posuere sem, at tristique dolor eleifend vitae. Curabitur vitae mauris vitae orci placerat suscipit eu hendrerit felis. Suspendisse vestibulum lacinia lectus, eget adipiscing dui lobortis eu.</div>
+</article>
