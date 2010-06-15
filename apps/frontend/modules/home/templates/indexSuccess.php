9bb3e05ffbd5989e3811ffdd86a86af26a077051
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-06-15 21:42:15 +0200

    Add the homepage slideshow js code

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,10 +1,16 @@
 <?php use_stylesheet('home') ?>
+<?php use_javascript('jquery') ?>
+<?php use_javascript('home-slideshow') ?>
 
-<?php echo image_tag('slideshow_placeholder.jpg') ?>
+<ol class="slideshow">
+    <li><?php echo image_tag('slideshow_placeholder.jpg') ?></li>
+    <li><?php echo image_tag('slideshow_placeholder2.jpg') ?></li>
+    <li><?php echo image_tag('slideshow_placeholder3.jpg') ?></li>
+</ol>
 
 <h2>news</h2>
 <article class="news">
-    <span class="date">03.09.2009</span>
+    <span class="date">13.06.2010</span>
     <div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed sem nunc. Mauris sem tellus, faucibus eu accumsan sed, mattis sit amet nunc. Nunc sed est eget enim auctor viverra. Integer quis sem tempus est ornare porttitor. Suspendisse potenti. In a mauris orci, vel auctor augue. In sit amet dui ac dui gravida posuere. Morbi eu ante enim, eu convallis metus. Fusce et leo nunc, nec feugiat tortor. Integer nulla risus, faucibus sed mollis eget, placerat vitae purus. Etiam dictum, ante a suscipit fermentum, dolor diam ultrices libero, vel posuere ligula nibh in ipsum. Nam ac justo nec est mollis imperdiet ut id nunc. Mauris et elit nisl, quis feugiat justo. Quisque massa magna, pulvinar non sodales ac, tincidunt nec dui. Aliquam dignissim tellus in neque sodales id fringilla dolor faucibus. Donec ultrices posuere sem, at tristique dolor eleifend vitae. Curabitur vitae mauris vitae orci placerat suscipit eu hendrerit felis. Suspendisse vestibulum lacinia lectus, eget adipiscing dui lobortis eu.</div>
 </article>
 <article class="news">
