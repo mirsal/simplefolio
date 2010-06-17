6a60601f4f8e0ba5295e2faf8566a97180cbb71d
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-06-17 08:54:55 +0200

    Add the gallery module and a rss icon next to the news section header

---
+++ apps/frontend/modules/gallery/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * gallery actions.
+ *
+ * @package    simplefolio
+ * @subpackage gallery
+ * @author     Mirsal Ennaime
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class galleryActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+
+  public function executeShow(sfWebRequest $request)
+  {
+  }
+}
---
+++ apps/frontend/modules/gallery/templates/showSuccess.php
@@ -0,0 +1,18 @@
+<?php use_stylesheet('gallery') ?>
+
+<h2>Illustrations</h2>
+
+<ul class="thumbnails">
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder2.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder3.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder4.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder5.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder6.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder7.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder8.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder9.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder10.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder11.jpg') ?></a></li>
+    <li><a href="#"><?php echo image_tag('thumbnail_placeholder12.jpg') ?></a></li>
+</ul>
